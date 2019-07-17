82e2db0781ff19a468bba0cb7defaa83e30ea450
vagrant <vagrant@homestead>
2019-07-17 08:15:51 +0000

    裁剪头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -27,7 +27,7 @@ class UsersController extends Controller
 //        dd($request->avatar);
         $data = $request->all();
         if ($request->avatar) {
-            $result = $uploader->save($request->avatar,'avatar',$user->id);
+            $result = $uploader->save($request->avatar,'avatar',$user->id,416);
             if ($result){
                 $data['avatar'] = $result['path'];
             }
