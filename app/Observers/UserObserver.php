ea3565018a953d1e9f84eaa1dae7fc090f2d88fa
vagrant <vagrant@homestead>
2019-07-19 02:32:08 +0000

    ok

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -9,16 +9,7 @@ use App\Models\User;
 
 class UserObserver
 {
-    public function creating(User $user)
-    {
-        //
-    }
-
-    public function updating(User $user)
-    {
-        //
-    }
-
+    
     public function saving(User $user)
     {
         //这样写扩展性更高，只有在空的时候才指定默认头像
