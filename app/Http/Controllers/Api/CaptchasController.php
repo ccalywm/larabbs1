744d50a8592aaa63eceb14dffa481cb230f199e8
vagrant <vagrant@homestead>
2019-07-20 03:44:43 +0000

    手机注册

--- app/Http/Controllers/Api/CaptchasController.php
+++ app/Http/Controllers/Api/CaptchasController.php
@@ -2,26 +2,25 @@
 
 namespace App\Http\Controllers\Api;
 
-use Gregwar\Captcha\CaptchaBuilder;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Gregwar\Captcha\CaptchaBuilder;
 use App\Http\Requests\Api\CaptchaRequest;
 
 class CaptchasController extends Controller
 {
-    public function store(CaptchaRequest $request,CaptchaBuilder $builder)
+    public function store(CaptchaRequest $request, CaptchaBuilder $captchaBuilder)
     {
-        $key = 'captcha-'.str_random(15);
+        $key = 'captcha-'.str_random(5);
         $phone = $request->phone;
 
-        $captcha = $builder->build();
-        $expiredAt = now()->addMinute(2);
-        \Cache::put($key,['phone' => $phone, 'code' => $captcha->getPhrase(),$expiredAt]);
+        $captcha = $captchaBuilder->build();
+        $expiredAt = now()->addMinutes(2);
+        \Cache::put($key, ['phone' => $phone, 'code' => $captcha->getPhrase()], $expiredAt);
 
         $result = [
             'captcha_key' => $key,
             'expired_at' => $expiredAt->toDateTimeString(),
-            'captcha_image_content' => $captcha->inline(),
+            'captcha_image_content' => $captcha->inline()
         ];
 
         return $this->response->array($result)->setStatusCode(201);
